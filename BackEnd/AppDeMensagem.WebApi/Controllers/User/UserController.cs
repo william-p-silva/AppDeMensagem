dd2552387088f7f6e4e818a0ee21c5627db6754a
william-p-silva <josepereirawilliam@gmail.com>
2026-07-17 17:10:08 -0300

    feat: Controller de registo de user

---
+++ BackEnd/AppDeMensagem.WebApi/Controllers/User/UserController.cs
@@ -0,0 +1,19 @@
+﻿
+using AppDeMensagem.Application.DTOs.User;
+using AppDeMensagem.Application.UseCases.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppDeMensagem.WebApi.Controllers.User;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController(RegisterUseCase registerUseCase) : ControllerBase
+{
+    [HttpPost("post")]
+    public async Task<IActionResult> RegisterUser(RequestRegister request)
+    {
+        var user = registerUseCase.Execute(request);
+        return Ok(user);
+    }
+    
+}
