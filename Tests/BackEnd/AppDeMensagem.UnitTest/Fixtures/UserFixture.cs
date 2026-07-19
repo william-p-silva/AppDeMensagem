4b39a3b4f7b4ba51a527c802cff266de43e8f677
william-p-silva <josepereirawilliam@gmail.com>
2026-07-18 21:04:42 -0300

    feat: Adicionando projeto de testes unitarios e teste de registro de usuario

---
+++ Tests/BackEnd/AppDeMensagem.UnitTest/Fixtures/UserFixture.cs
@@ -0,0 +1,42 @@
+﻿
+using AppDeMensagem.Application.DTOs.User;
+using AppDeMensagem.Domain.Entity;
+using AppDeMensagem.Domain.Enum;
+
+namespace AppDeMensagem.UnitTest.Fixtures;
+
+internal class UserFixture
+{
+    public Usuario CreateUserFake(
+        string email,
+        string userName,
+        string password,
+        PerfilUser profile
+        )
+    {
+        Usuario user = new Usuario(
+            email: email,
+            userName: userName,
+            passwordHash: password,
+            userProfile: profile
+            );
+
+        return user;
+    }
+
+    public RequestRegister CreateRequestRegisterUser(
+        string email,
+        string userName,
+        string password,
+        PerfilUser profile
+        )
+    {
+        return new RequestRegister
+        {
+            Email = email,
+            Name = userName,
+            PassoWord = password,
+            Profile = profile
+        };
+    }
+}
---
+++ Tests/BackEnd/AppDeMensagem.UnitTest/Tests/UserTests/RegisterUserTest.cs
@@ -0,0 +1,56 @@
+﻿
+
+using AppDeMensagem.Application.DTOs.User;
+using AppDeMensagem.Application.Interfaces.Repositorys;
+using AppDeMensagem.Application.Interfaces.Security;
+using AppDeMensagem.Application.UseCases.User;
+using AppDeMensagem.Domain.Entity;
+using AppDeMensagem.Domain.Enum;
+using AppDeMensagem.UnitTest.Fixtures;
+using Moq;
+
+namespace AppDeMensagem.UnitTest.Tests.UserTests;
+
+public class RegisterUserTest
+{
+    private readonly UserFixture userFixture = new UserFixture();
+    private readonly Mock<IUserRepository> _userRepositoryMock = new();
+    private readonly Mock<IPasswordHasher> _passwordHasherMock = new();
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+
+    [Fact]
+    public async Task ReturnSuccess_OnValidDados()
+    {
+        //Arrange
+        string emailFake = "testvalid@gmail.com";
+        string userNameFake = "Name Valid of Silva";
+        string passwordFake = "123";
+        PerfilUser profile = PerfilUser.User;
+
+        RequestRegister requestRegisterFake = userFixture.CreateRequestRegisterUser(
+            email: emailFake,
+            userName: userNameFake,
+            password: passwordFake,
+            profile: profile
+            );
+
+        //Act
+        _passwordHasherMock.Setup(x => x.HashPassword(It.IsAny<string>())).Returns(passwordFake);
+        _userRepositoryMock.Setup(x => x.FindByEmail(requestRegisterFake.Email)).ReturnsAsync((Usuario?) null);
+
+        var registerUseCase = new RegisterUseCase(
+            _userRepositoryMock.Object,
+            _passwordHasherMock.Object,
+            _unitOfWorkMock.Object
+            );
+
+        var user = await registerUseCase.Execute(requestRegisterFake);
+
+        //Assert
+        Assert.NotNull(user);
+        Assert.Equal(user, "Registration successfuly completed. ");
+        _passwordHasherMock.Verify(x => x.HashPassword(It.IsAny<string>()), Times.Once());
+        _userRepositoryMock.Verify(x => x.FindByEmail(requestRegisterFake.Email), Times.Once());
+        _unitOfWorkMock.Verify(x => x.CommitAsync(), Times.Once());
+    }
+}
